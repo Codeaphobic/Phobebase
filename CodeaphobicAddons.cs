80cfe0ddcfd3f5dcb8c3ce936f20545bfa0805b3
Chooken <joshuawereszczuk@gmail.com>
2022-08-19 10:43:23 +1000

    Update CodeaphobicAddons.cs
    Added GPU Compute Class
    makes ComputeShaders a little easier

--- CodeaphobicAddons.cs
+++ CodeaphobicAddons.cs
@@ -79,13 +79,13 @@ namespace Codeaphobic
 		private static Dictionary<Transform, List<Task>> _activeTweens = new Dictionary<Transform, List<Task>>();
 		private static Dictionary<Task, CancellationTokenSource> _cancelTokens = new Dictionary<Task, CancellationTokenSource>();
 
-#if UNITY_EDITOR
-		[RuntimeInitializeOnLoadMethod]
-		static void RuntimeStart()
-		{
-			EditorApplication.playModeStateChanged += StopAllTweens;
-		}
-#endif
+		#if UNITY_EDITOR
+				[RuntimeInitializeOnLoadMethod]
+				static void RuntimeStart()
+				{
+					EditorApplication.playModeStateChanged += StopAllTweens;
+				}
+		#endif
 		public static void TweenTo(this Transform transform, Vector3 lerpPosition, Quaternion lerpRotation, Vector3 lerpScale, float time, AnimationCurve curve = null)
 		{
 			if (curve == null) curve = AnimationCurve.Linear(0, 0, 1, 1);
@@ -562,6 +562,173 @@ namespace Codeaphobic
 		public Vector3 PointOnCurve(float time)
 		{
 			return (1f - time) * (1f - time) * (1f - time) * point1 + 3 * ((1f - time) * (1f - time)) * time * weight1 + 3 * (1f - time) * (time * time) * weight2 + time * time * time * point2;
+		}
+	}
+
+	public class GPUCompute
+	{
+		private ComputeShader m_shader;
+		public ComputeShader shader {
+			get { return m_shader; }
+			private set { m_shader = value; }
+		}
+
+		private Dictionary<string, ComputeBuffer> m_buffers = new List<ComputeBuffer>();
+		public Dictionary<string, ComputeBuffer> buffers {
+			get { return m_buffers; }
+			private set { m_buffers = value; }
+		}
+
+		private int m_bufferLocation = 0;
+
+		public static GPUCompute Create(ComputeShader shader)
+		{
+			this.shader = shader
+			return this;
+		}
+
+		#region Add Returnable Data
+
+		public GPUCompute AddTexture(ref RenderTexture texture, string textureName)
+		{
+			shader.SetTexture(bufferLocation, textureName, texture);
+			return this;
+		}
+
+		public GPUCompute CreateTexture(string textureName, Vector2 dimensions, int depth, out RenderTexture texture)
+		{
+			texture = new RenderTexture(dimensions.x, dimensions.y, depth);
+			texture.enableRandomWrite = true;
+			texture.Create();
+
+			shader.SetTexture(bufferLocation, textureName, texture);
+			return this;
+		}
+
+		public GPUCompute AddTexture(ref RenderTexture texture, string textureName, int bufferLocation)
+		{
+			shader.SetTexture(bufferLocation, textureName, texture);
+			return this;
+		}
+
+		public GPUCompute CreateTexture(string textureName, Vector2 dimensions, int depth, int bufferLocation, out RenderTexture texture)
+		{
+			texture = new RenderTexture(dimensions.x, dimensions.y, depth);
+			texture.enableRandomWrite = true;
+			texture.Create();
+
+			shader.SetTexture(bufferLocation, textureName, texture);
+			return this;
+		}
+
+		public GPUCompute AddBuffer<T>(string name, ref T[] bufferObjects, int bufferObjectSize)
+		{
+			ComputeBuffer buffer = new ComputeBuffer(bufferObjects.Length, bufferObjectSize);
+			buffer.SetData(bufferObjects);
+			buffers.Add(name, buffer);
+
+			shader.SetBuffer(0, name, buffer);
+			return this;
+		}
+
+		public GPUCompute AddBuffer<T>(string name, ref T[] bufferObjects, int bufferObjectSize, int bufferLocation)
+		{
+			ComputeBuffer buffer = new ComputeBuffer(bufferObjects.Length, bufferObjectSize);
+			buffer.SetData(bufferObjects);
+			buffers.Add(name, buffer);
+
+			shader.SetBuffer(bufferLocation, name, buffer);
+			return this;
+		}
+
+		#endregion
+		#region Adding Data
+
+		public GPUCompute AddFloat(string name, float value)
+		{
+			texture.SetFloat(name, value);
+			return this;
+		}
+
+		public GPUCompute AddFloats(string name, float[] values)
+		{
+			texture.SetFloats(name, values);
+			return this;
+		}
+
+		public GPUCompute AddInt(string name, int value)
+		{
+			texture.SetInt(name, value);
+			return this;
+		}
+
+		public GPUCompute AddInts(string name, int[] values)
+		{
+			texture.SetInts(values);
+			return this;
+		}
+
+		public GPUCompute AddVector(string name, Vector4 value)
+		{
+			texture.SetVector(name, value);
+			return this;
+		}
+
+		public GPUCompute AddVectorArray(string name, Vector4[] values)
+		{
+			texture.SetVectorArray(name, values);
+			return this;
+		}
+
+		public GPUCompute AddMatrix(string name, Matrix4x4 value)
+		{
+			texture.SetMatrix(name, value);
+			return this;
+		}
+
+		public GPUCompute AddMatrixArray(string name, Matrix4x4[] values)
+		{
+			texture.SetMatrixArray(name, values);
+			return this;
+		}
+
+		public GPUCompute AddBool(string name, bool value)
+		{
+			texture.SetBool(name, value);
+			return this;
+		}
+
+		#endregion
+		#region Retreive Data
+
+		public GPUCompute Compute(Vector2 parallelizationAxis)
+		{
+			shader.Dispatch(0, parallelizationAxis.x, parallelizationAxis.y, 1);
+			return this;
+		}
+
+		public GPUCompute GetData<T>(string name, ref T[] bufferObjects) 
+		{
+			buffers[name].GetData(bufferObjects);
+			return this;
+		}
+
+		#endregion
+
+		public GPUCompute DisposeBuffer(string name) 
+		{
+			buffers[name].Dispose();
+			buffers.Remove(name);
+		}
+
+		public GPUCompute DisposeBuffers()
+		{
+			foreach (ComputeBuffer buffer in buffers) 
+			{
+				buffer.Dispose();
+			}
+			buffers.Clear();
+			return this;
 		}
 	}
 }
